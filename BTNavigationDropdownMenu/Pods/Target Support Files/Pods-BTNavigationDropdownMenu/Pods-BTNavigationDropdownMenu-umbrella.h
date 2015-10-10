7e6b8f2ca8a3876fade99aac91f7a98283a55e09
elad-rahamim <elad@eready.co.il>
2015-10-10 20:09:46 +0300

    BTNavigationDropdownMenu project
    https://github.com/PhamBaTho/BTNavigationDropdownMenu
    https://cocoapods.org/?q=BTNavigationDropdownMenu

---
+++ BTNavigationDropdownMenu/Pods/Target Support Files/Pods-BTNavigationDropdownMenu/Pods-BTNavigationDropdownMenu-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BTNavigationDropdownMenuVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BTNavigationDropdownMenuVersionString[];
+
---
+++ BTNavigationDropdownMenu/Pods/Target Support Files/Pods-BTNavigationDropdownMenuTests/Pods-BTNavigationDropdownMenuTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BTNavigationDropdownMenuTests : NSObject
+@end
+@implementation PodsDummy_Pods_BTNavigationDropdownMenuTests
+@end
